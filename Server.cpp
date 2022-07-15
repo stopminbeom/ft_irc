188648c70a324aedbe706a641e2ea9c4bd2e298b
stopminbeom <mseo@student.42seoul.kr>
2022-07-15 20:58:56 +0900

    Feat: poll setup

--- Server.cpp
+++ Server.cpp
@@ -6,8 +6,12 @@ using std::cerr;
 
 Server::Server(int _port, std::string pw): port(_port), password(pw) {}
 
-Server::~Server()
-{
+Server::~Server() {
+	for (int i = 0; i < POLL_NUM; ++i) {
+		if (poll_arr[i].fd != -1) {
+			close(poll_arr[i].fd);
+		}
+	}
 }
 
 int	Server::setup() {
@@ -28,9 +32,38 @@ int	Server::setup() {
 		cerr << "[Error] Can't listen server socket" << endl;
 		return 1;
 	}
+	poll_arr[0].fd = server_socket;
+	poll_arr[0].events = POLLIN;
+	poll_arr[0].revents = 0;
+
+	for (int i = 1; i < POLL_NUM; i++)
+		poll_arr[i].fd = -1;
+
 	return 0;
 }
 
 int	Server::run() {
-	
+	while (1) {
+		if (poll(poll_arr, POLL_NUM, -1)) {
+			if (poll_arr[0].revents == POLLIN) {
+				bzero(&client_addr, sizeof(client_addr));
+				int client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &addrsize);
+
+				for (int i = 1; i < POLL_NUM; i++) {
+					if (poll_arr[i].fd == -1) {
+						poll_arr[i].fd = client_socket;
+						poll_arr[i].events = POLLIN;
+						poll_arr[i].revents = 0;
+						break;
+					}
+				}
+			}
+
+			for (int i = 1; i < POLL_NUM; i++) {
+				if (poll_arr[i].revents == POLLIN) {
+					// recv 함수 부분
+				}
+			}
+		}
+	}
 }
--- Server.hpp
+++ Server.hpp
@@ -1,18 +1,24 @@
 #ifndef SERVER_HPP
-#define SERVER_HPP
+# define SERVER_HPP
 
-#include <string>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <iostream>
-#include <stdlib.h>
+# include <string>
+# include <sys/socket.h>
+# include <arpa/inet.h>
+# include <iostream>
+# include <stdlib.h>
+# include <poll.h>
+# include <unistd.h>
+
+# define POLL_NUM	100
 
 class Server {
 	private:
 		int	port;
 		int	server_socket;
 		std::string	password;
-		struct sockaddr_in server_addr;
+		struct sockaddr_in	server_addr, client_addr;
+		struct pollfd	poll_arr[POLL_NUM];
+		socklen_t	addrsize;
 
 	public:
 		Server(int _port, std::string pw);
